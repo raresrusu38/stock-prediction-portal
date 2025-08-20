edf5d22b167337526939321baf4391e2c68f5e4b
raresrusu38 <rares.rusu29@gmail.com>
2025-08-20 15:21:54 +0300

    second commit

---
+++ frontend-react/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+        <footer className='py-3 my-3'>
+            <hr className='border-bottom' />
+            <p className='text-center text-light'>&copy; 2025 - Built with ❤️ by Rares Rusu</p>
+
+        </footer>
+    </>
+  )
+}
+
+export default Footer
